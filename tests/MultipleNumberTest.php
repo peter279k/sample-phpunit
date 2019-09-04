cfbc167cef2a9e1d71c231ec13f6d7376044196a
peter279k <peter279k@gmail.com>
2019-09-05 02:07:12 +0800

    Enhance Multiple, Minus and their tests methods

--- tests/MultipleNumberTest.php
+++ tests/MultipleNumberTest.php
@@ -2,6 +2,7 @@
 
 namespace Lee\Tests;
 
+use InvalidArgumentException;
 use Lee\MultipleNumber;
 use PHPUnit\Framework\TestCase;
 
@@ -9,10 +10,28 @@ class MultipleNumberTest extends TestCase
 {
     public function testMultipleTwoNumbersCanBeMultiple()
     {
-        $addition = new MultipleNumber();
-        $result = $addition->multipleTwoNumbers(3, 2);
+        $multiple = new MultipleNumber();
+        $result = $multiple->multipleTwoNumbers(3, 2);
         $expected = 6;
 
         $this->assertSame($expected, $result);
+    }
+
+    public function testMultipleNumbersCanBeMultiple()
+    {
+        $multiple = new MultipleNumber();
+        $result = $multiple->multipleNumbers(1, 2, 3);
+        $expected = 6;
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function testMultipleArrayNumbersThrowInvalidArgumentException()
+    {
+        $multiple = new MultipleNumber();
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $multiple->multipleNumbers([1], [2], [3]);
     }
 }
